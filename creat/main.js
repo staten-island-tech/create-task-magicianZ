3a64b90bddc023aec88d6e7af737790d82d6df7d
Ethan De Jesus <124072155+magicianZ@users.noreply.github.com>
2024-01-18 13:13:10 -0500

    w

--- creat/main.js
+++ creat/main.js
@@ -1,45 +1,73 @@
 import './style.css'
-let mode = 0
+let theWord1 = ""
 document.querySelector('#app').innerHTML = `
 
 
   <button id="easy"> Easy Mode </button>
-  <button id="medium"> Medium Code </button>
+  <button id="medium"> Medium Mode </button>
   <button id="hard"> Hard Mode </button>
+  <form action=""id="forum">
+    <input type="text" id="input">
+    <label for="text">What letter are you guessing?</label>
+    </form>
+    <button id="theirguessbutton"> Click to submit your guess.. </button>
+
+  <div id="box"></div>
 `
 
-const URL = "https://random-word-api.herokuapp.com/word"
+let URL = `https://random-word-api.herokuapp.com/word`
 
 
 let Domselectors = {
   easymode:document.querySelector("#easy"),
   mediummode:document.querySelector("#medium"),
   hardmode:document.querySelector("#hard"),
+  theirguess:document.querySelector("#input"),
+  theirguessbutton:document.querySelector("#theirguessbutton"),
 
 }
 
-Domselectors.easymode.addEventListener("click",function(){
-  mode = easymode
+Domselectors.easymode.addEventListener("click",async function(){
+  URL =  `https://random-word-api.herokuapp.com/word?length=5`
+  let fetching = await fetch(URL);
+  let RealUrl = await fetching.json();
+  let theWord = RealUrl.forEach((something)=>theWord1 = something)
+  console.log(theWord1)
+
 })
 
+Domselectors.mediummode.addEventListener("click", async function(){
+  URL = `https://random-word-api.herokuapp.com/word?length=7`
+  let fetching = await fetch(URL);
+  let RealUrl = await fetching.json();
+  let theWord = RealUrl.forEach((something)=>theWord1 = something)
+  console.log(theWord1)
+})
 
+Domselectors.hardmode.addEventListener("click",async function(){
+  URL = `https://random-word-api.herokuapp.com/word?length=15`
+  let fetching = await fetch(URL);
+  let RealUrl = await fetching.json();
+  let theWord = RealUrl.forEach((something)=>theWord1 = something)
+  console.log(theWord1)
+})
 
-
-
-
-
-
-
-
-async function something(URL){
-  try{
-    let fetching = await fetch(URL);
-    let RealUrl = await fetching.json();
-    let theWord = RealUrl.forEach((something)=> console.log(something))
+Domselectors.theirguessbutton.addEventListener("click",async function(){
+  for (let i=0; i<theWord1.length; i++){
+    if (Domselectors.theirguess.value[0]===theWord1[i]){
+      console.log(Domselectors.theirguess.value[0],theWord1[i])
+    }
 
   }
-  catch{
-    console.log('catched')
+  
+
+
   }
-}
-something(URL)
+)
+
+
+
+
+
+
+
