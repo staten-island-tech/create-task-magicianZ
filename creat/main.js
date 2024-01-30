9ba323c0b1efef9fd18fc1e5b9a1b5f69d7fadbe
Ethan De Jesus <ethanawesome702@gmail.com>
2024-01-29 19:37:24 -0500

    e

--- creat/main.js
+++ creat/main.js
@@ -1,4 +1,5 @@
 import './style.css'
+let answer = []
 let theWord1 = ""
 document.querySelector('#app').innerHTML = `
 
@@ -27,12 +28,29 @@ let Domselectors = {
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Domselectors.easymode.addEventListener("click",async function(){
   URL =  `https://random-word-api.herokuapp.com/word?length=5`
   let fetching = await fetch(URL);
   let RealUrl = await fetching.json();
-  let theWord = RealUrl.forEach((something)=>theWord1 = something)
+  let theWord = RealUrl.forEach((something)=> theWord1 = something)
   console.log(theWord1)
+  for (let i = 0; i < theWord1.length; i++){
+    answer[i] = "_"
+  }
 
 })
 
@@ -40,7 +58,7 @@ Domselectors.mediummode.addEventListener("click", async function(){
   URL = `https://random-word-api.herokuapp.com/word?length=7`
   let fetching = await fetch(URL);
   let RealUrl = await fetching.json();
-  let theWord = RealUrl.forEach((something)=>theWord1 = something)
+  RealUrl.forEach((something)=>theWord1 = something)
   console.log(theWord1)
 })
 
@@ -48,17 +66,28 @@ Domselectors.hardmode.addEventListener("click",async function(){
   URL = `https://random-word-api.herokuapp.com/word?length=15`
   let fetching = await fetch(URL);
   let RealUrl = await fetching.json();
-  let theWord = RealUrl.forEach((something)=>theWord1 = something)
+  RealUrl.forEach((something)=>theWord1 = something)
   console.log(theWord1)
 })
 
 Domselectors.theirguessbutton.addEventListener("click",async function(){
-  for (let i=0; i<theWord1.length; i++){
+
+
+  for (let i = 0; i <theWord1.length; i++){
+    if (Domselectors.theirguess.value[0]===theWord1[i]){
+      answer[i] = Domselectors.theirguess.value[0]
+    }
+  }
+  if(!answer.includes("_")){
+    console.log("u win")
+  }
+  console.log(answer)
+  /* for (let i=0; i<theWord1.length; i++){
     if (Domselectors.theirguess.value[0]===theWord1[i]){
       console.log(Domselectors.theirguess.value[0],theWord1[i])
     }
 
-  }
+  } */
   
 
 
