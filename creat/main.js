c435c8b7f1df21f7a4aa96f168d0fed9b8cee8f2
Ethan De Jesus <124072155+magicianZ@users.noreply.github.com>
2024-01-30 13:14:00 -0500

    done lowkey

--- creat/main.js
+++ creat/main.js
@@ -2,9 +2,11 @@ import './style.css'
 let answer = []
 let theWord1 = ""
 let missed = []
+let count = 0
 document.querySelector('#app').innerHTML = `
 
-
+  <h1> hangman </h1>
+  <h3> only use lower case and pick a mode before u play </h3>
   <button id="easy"> Easy Mode </button>
   <button id="medium"> Medium Mode </button>
   <button id="hard"> Hard Mode </button>
@@ -15,6 +17,7 @@ document.querySelector('#app').innerHTML = `
     <button id="theirguessbutton"> Click to submit your guess.. </button>
 
   <div id="box"></div>
+  <div id="box1"></div>
 `
 
 let URL = `https://random-word-api.herokuapp.com/word`
@@ -26,7 +29,8 @@ let Domselectors = {
   hardmode:document.querySelector("#hard"),
   theirguess:document.querySelector("#input"),
   theirguessbutton:document.querySelector("#theirguessbutton"),
-  box:document.querySelector("#box")
+  box:document.querySelector("#box"),
+  box1:document.querySelector("#box1")
 
 }
 
@@ -59,41 +63,47 @@ Domselectors.easymode.addEventListener("click",async function(){
 
 Domselectors.mediummode.addEventListener("click", async function(){
   answer.pop()
-  URL = `https://random-word-api.herokuapp.com/word?length=7`
+  URL =  `https://random-word-api.herokuapp.com/word?length=7`
   let fetching = await fetch(URL);
   let RealUrl = await fetching.json();
-  RealUrl.forEach((something)=>theWord1 = something)
+  let theWord = RealUrl.forEach((something)=> theWord1 = something)
   console.log(theWord1)
+  for (let i = 0; i < theWord1.length; i++){
+    answer[i] = "_"
+  }
+ 
 })
 
 Domselectors.hardmode.addEventListener("click",async function(){
   answer.pop()
-  URL = `https://random-word-api.herokuapp.com/word?length=15`
+  URL =  `https://random-word-api.herokuapp.com/word?length=15`
   let fetching = await fetch(URL);
   let RealUrl = await fetching.json();
-  RealUrl.forEach((something)=>theWord1 = something)
+  let theWord = RealUrl.forEach((something)=> theWord1 = something)
   console.log(theWord1)
+  for (let i = 0; i < theWord1.length; i++){
+    answer[i] = "_"
+  }
 })
 
 Domselectors.theirguessbutton.addEventListener("click",async function(){
+  count++
 
 
   for (let i = 0; i <theWord1.length; i++){
     if (Domselectors.theirguess.value[0]===theWord1[i]){
       answer[i] = Domselectors.theirguess.value[0]
     }
-    if (Domselectors.theirguess.value[0]!=theWord1[i]){
-        if(!missed.includes(Domselectors.theirguess.value)){
-          missed.push(Domselectors.theirguess.value[0])
-        }
-    }
   }
   if(!answer.includes("_")){
-    Domselectors.box.innerHTML+="u win"
+    Domselectors.box1.innerHTML=`u win and you took ${count} tries`
     console.log("u win")
   }
   //Domselectors.box.innerHTML += answer
-  Domselectors.box.innerHTML += missed
+  console.log(answer)
+  console.log(theWord1)
+
+  Domselectors.box.innerHTML = answer
   /* for (let i=0; i<theWord1.length; i++){
     if (Domselectors.theirguess.value[0]===theWord1[i]){
       console.log(Domselectors.theirguess.value[0],theWord1[i])
